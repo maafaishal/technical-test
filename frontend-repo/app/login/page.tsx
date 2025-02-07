7c6298bdc6953993e8133c59502e812581c7244b
Turbobot <turbobot@vercel.com>
2025-02-07 23:39:25 +0700

    feat(create-turbo): apply official-starter transform

---
+++ frontend-repo/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@mui/material/Button";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>LoginPage</h1>
+      <Button variant="contained">Hello world</Button>
+    </div>
+  );
+}
